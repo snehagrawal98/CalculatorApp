f8942065d57fb134ad130cad3f4fdf05ac7b281e
Sneha Agrawal <snehagrawal98@gmail.com>
2022-01-16 01:46:40 +0530

    Initial Commit

---
+++ CalculatorApp/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  CalculatorApp
+//
+//  Created by mnameit on 16/01/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
